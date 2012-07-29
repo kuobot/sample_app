dd30d4ab19e9bdf55eed7526cbed9753be6706b9
Joann <joann.kuo@gmail.com>
2012-07-28 20:11:46 -0700

    Add About Us page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -13,4 +13,10 @@ describe "StaticPages" do
 			page.should have_content ('Help')
    		end
   	end
+  	   describe "About page" do
+		it "should have the content 'About Us'" do
+			visit '/static_pages/about'
+			page.should have_content ('About Us')
+   		end
+  	end
 end
