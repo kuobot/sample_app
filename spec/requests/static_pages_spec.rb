0805b06b93d30522b929acc1b60ebfb4ba240f89
Joann <joann.kuo@gmail.com>
2012-07-28 21:54:44 -0700

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,22 +1,41 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
 	describe "Home page" do
-		it "should have the content 'Sample App'" do
+	
+  	it "should have the h1 'Home'" do
 			visit '/static_pages/home'
-			page.should have_content ('Sample App')
-    	end
-  	end
-    describe "Help page" do
-		it "should have the content 'Help'" do
+			page.should have_selector('h1', :text => 'Home')
+    end
+    	it "should have the right title" do
+    		visit '/static_pages/home'
+			page.should have_selector('title', :text => "Kittens Rule, Inc. | Home")
+		end
+  end
+  
+  describe "Help page" do
+
+		it "should have the h1 'Help'" do
 			visit '/static_pages/help'
-			page.should have_content ('Help')
-   		end
+			page.should have_selector('h1', :text => 'Help')
+   	end
+   		it "should have the right title" do
+    		visit '/static_pages/help'
+			page.should have_selector('title', :text => "Kittens Rule, Inc. | Help")
   	end
-  	   describe "About page" do
-		it "should have the content 'About Us'" do
+	end
+  
+  describe "About page" do
+		
+    it "should have the h1 'About Us'" do
 			visit '/static_pages/about'
-			page.should have_content ('About Us')
-   		end
-  	end
+			page.should have_selector('h1',	:text => 'About Us')
+		end
+
+   		it "should have the right title" do
+    		visit '/static_pages/about'
+			page.should have_selector('title', :text => "Kittens Rule, Inc. | About Us")
+		end
+ 	end
 end
