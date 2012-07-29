7c427493841ab77b991d4bcd7c0285079892c7b6
Joann <joann.kuo@gmail.com>
2012-07-28 22:03:12 -0700

    Add Contact Us page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -38,4 +38,17 @@ describe "StaticPages" do
 			page.should have_selector('title', :text => "Kittens Rule, Inc. | About Us")
 		end
  	end
+
+    describe "Contact page" do
+    
+    it "should have the h1 'Contact Us'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact Us')
+    end
+
+      it "should have the right title" do
+        visit '/static_pages/contact'
+      page.should have_selector('title', :text => "Kittens Rule, Inc. | Contact Us")
+    end
+  end
 end
