2189b5f68c734c1b566739389f3e633d388ffbcc
Joann <joann.kuo@gmail.com>
2012-08-04 19:58:43 -0700

    Fix failing tests and add paths

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,15 +7,15 @@ describe "StaticPages" do
 	describe "Home page" do
 	
   	it "should have the h1 'Sample App'" do
-			visit '/static_pages/home'
+			visit root_path
 			page.should have_selector('h1', :text => 'Sample App')
     end
   	it "should have the base title" do
-   		visit '/static_pages/home'
+   		visit root_path
     page.should have_selector('title', :text => "Kittens Rule, Inc.")
 		end
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
     page.should_not have_selector('title', :text => ' | :Home')  
     end
   end
@@ -23,11 +23,11 @@ describe "StaticPages" do
   describe "Help page" do
 
 		it "should have the h1 'Help'" do
-			visit '/static_pages/help'
+			visit help_path
 			page.should have_selector('h1', :text => 'Help')
    	end
-   		it "should have the right title" do
-    		visit '/static_pages/help'
+ 		it "should have the right title" do
+  		visit help_path
 			page.should have_selector('title', :text => "#{base_title} | Help")
   	end
 	end
@@ -35,12 +35,12 @@ describe "StaticPages" do
   describe "About page" do
 		
     it "should have the h1 'About Us'" do
-			visit '/static_pages/about'
+			visit about_path
 			page.should have_selector('h1',	:text => 'About Us')
 		end
 
-   		it "should have the right title" do
-    		visit '/static_pages/about'
+   	it "should have the right title" do
+    	visit about_path
 			page.should have_selector('title', :text => "#{base_title} | About Us")
 		end
  	end
@@ -48,13 +48,13 @@ describe "StaticPages" do
     describe "Contact page" do
     
     it "should have the h1 'Contact Us'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('h1', :text => 'Contact Us')
     end
 
       it "should have the right title" do
-        visit '/static_pages/contact'
-      page.should have_selector('title', :text => "Kittens Rule, Inc. | Contact Us")
+        visit contact_path
+      page.should have_selector('title', :text => "#{base_title} | Contact Us")
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,6 +37,9 @@ Spork.prefork do
     # automatically. This will be the default behavior in future versions of
     # rspec-rails.
     config.infer_base_class_for_anonymous_controllers = false
+
+    config.include Rails.application.routes.url_helpers
+    
   end
 end
 
@@ -108,4 +111,7 @@ RSpec.configure do |config|
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
+
+  config.include Rails.application.routes.url_helpers
+    
 end
