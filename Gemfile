d1ba11f0bbdaeffa50c33c53c6618791a1d991fb
Joann <joann.kuo@gmail.com>
2012-08-09 22:05:45 -0700

    Finish user signup

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ group :development, :test do
   gem 'sqlite3', '1.3.5'
   gem 'rspec-rails', '2.10.0'
   gem 'guard-rspec', '0.5.5'
+  gem 'factory_girl_rails', '1.4.0'
 
 end
 
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name 					"Kitten"
+		email 					"kitten@gmail.com"
+		password				"kitten"
+		password_confirmation 	"kitten"
+	end
+end
