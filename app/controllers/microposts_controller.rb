376137c597f6d83360de506c0c7cfd632dba7ba9
Joann <joann.kuo@gmail.com>
2012-08-20 22:52:03 -0700

    Add user microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
-  before_filter :signed_in_user
+  before_filter :signed_in_user, only: [:create, :destroy]
+  before_filter :correct_user,   only: :destroy
 
   def create
     @micropost = current_user.microposts.build(params[:micropost])
@@ -7,10 +8,20 @@ class MicropostsController < ApplicationController
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
+      @feed_items = []
       render 'static_pages/home'
     end
   end
 
-  def destroy
+ def destroy
+    @micropost.destroy
+    redirect_to root_url
   end
+
+  private
+
+    def correct_user
+      @micropost = current_user.microposts.find_by_id(params[:id])
+      redirect_to root_url if @micropost.nil?
+    end
 end
